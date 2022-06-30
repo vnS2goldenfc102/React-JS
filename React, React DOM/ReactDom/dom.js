816ee7963f43cb9c11134aedafcc296a11b801bb
vnS2goldenfc102 <85670172+vnS2goldenfc102@users.noreply.github.com>
2022-06-30 14:55:31 +0700

    Add files via upload

---
+++ React, React DOM/ReactDom/dom.js
@@ -0,0 +1,3 @@
+// Virtural Dom
+// Virtural DOM là 1 Dom ảo, là một định dạng dữ liệu Java script dùng để thể hiện nội dung của DOM tại 1 thời điểm 
+// nhất định. Nó có tất cả thuộc tính giống Dom .Nhưng không có khả năng tương tác trên màn hình như dom
